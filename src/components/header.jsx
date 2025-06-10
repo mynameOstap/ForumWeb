5d00790d23eebe1ad00a1b39bfad4d488136d302
Ostap <ostduf2@gmail.com>
2025-06-10 22:00:23 +0300

    create header

---
+++ src/components/header.jsx
@@ -0,0 +1,28 @@
+import logo from "../assets/userIcon.svg"
+
+export const Header = () => {
+  return (
+    <header className="sticky top-0  z-50 bg-black w-full min-h-24">
+      <div className="p-8 text-white">
+        <div className="flex justify-between px-96 mt-6">
+          <h1 className="text-2xl">Random Musing.</h1>
+
+          <nav className="flex gap-12 items-center ">
+            <div className="flex gap-4">
+              <a href="#" className="hover:opacity-70">Home</a>
+              <a href="#" className="hover:opacity-70">Groups</a>
+              <a href="#" className="hover:opacity-70">About</a>
+              <a href="#" className="hover:opacity-70">Site Rules</a>
+              <a href="#" className="hover:opacity-70">Contact</a>
+              <a href="#" className="hover:opacity-70">Members</a>
+            </div>
+            <div className="flex gap-2 items-center">
+              <img src={logo} alt="user icon" className="size-6" />
+              <a href="#" className="hover:opacity-70">Log In</a>
+            </div>
+          </nav>
+        </div>
+      </div>
+    </header>
+  )
+}
---
+++ src/pages/home.jsx
@@ -0,0 +1,20 @@
+
+
+
+export const Home = () => {
+    return(
+        <>
+    <section>
+        <div class="w-screen h-screen bg-purple-600 ">
+            <div>fxsffs</div>
+        </div>
+    </section>
+    <section>
+        <div class="w-screen h-screen bg-blue-600 ">
+            <div>fxsffs</div>
+        </div>
+    </section>
+    </>
+    );
+    
+}
